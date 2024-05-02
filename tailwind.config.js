de76f57ca170882a6ea561f5342744d05d1fc63a
JayQinbits <jqurix352@gmail.com>
2024-05-02 08:42:47 -0700

    first update

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fea928",
+        secondary: "#ed8900",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
